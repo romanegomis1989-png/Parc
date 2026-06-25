60f0a8f9e806a32e9771a109390fc8305d454853
Romane <romane.gomis1989@gmail.com>
2026-06-25 11:42:14 +0200

    Corrections table utilise

--- data.sql
+++ data.sql
@@ -1,11 +1,12 @@
-CREATE DATABASE parc_informatique;
+DROP DATABASE IF EXISTS parc_informatique;
+CREATE DATABASE parc_informatique CHARACTER SET utf8mb4;
 USE parc_informatique;
 
-DROP TABLE IF EXISTS utilise;
-DROP TABLE IF EXISTS materiels;
-DROP TABLE IF EXISTS utilisateurs;
-DROP TABLE IF EXISTS marques;
-DROP TABLE IF EXISTS services;
+-- DROP TABLE IF EXISTS utilise;
+-- DROP TABLE IF EXISTS materiels;
+-- DROP TABLE IF EXISTS utilisateurs;
+-- DROP TABLE IF EXISTS marques;
+-- DROP TABLE IF EXISTS services;
 
 CREATE TABLE services (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -30,20 +31,21 @@ CREATE TABLE utilisateurs (
 CREATE TABLE materiels (
     id INT AUTO_INCREMENT PRIMARY KEY,
     no_serie VARCHAR(100) NOT NULL UNIQUE,
+    id_marque INT,
     modele VARCHAR(120),
     date_achat DATE,
-    prix_achat DECIMAL(),
-    id_marque INT,
+    prix_achat DECIMAL(8,2),
     FOREIGN KEY (id_marque) REFERENCES marques(id)
 );
 
 CREATE TABLE utilise (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     id_utilisateur INT,
     id_materiel INT,
-    date_debut DATE NOT NULL,
+    date_debut DATETIME NOT NULL,
     date_fin_prevue DATE,
     date_fin DATE,
-    PRIMARY KEY (id_utilisateur, id_materiel, date_debut),
+    UNIQUE (id_utilisateur, id_materiel, date_debut),
     FOREIGN KEY (id_utilisateur) REFERENCES utilisateurs(id),
     FOREIGN KEY (id_materiel) REFERENCES materiels(id)
 );
@@ -59,4 +61,4 @@ INSERT INTO marques (nom) VALUES
     ('Lenovo');
 
 INSERT INTO utilisateurs (nom, prenom, email, mot_de_passe, id_service) VALUES
-('Gomis',  'Romane', 'romane.gomis@parc.fr', 'hash_factice_1', 1),
+('Gomis',  'Romane', 'romane.gomis@parc.fr', 'hash_factice_1', 1)
